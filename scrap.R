bf6528cc71caf7a41c654894b2dfa2987dec1200
claytoncarpenter <claytonwc672@gmail.com>
2023-11-12 19:59:26 -0600

    fixed error

--- scrap.R
+++ scrap.R
@@ -11,6 +11,22 @@ penguins <- readr::read_csv('penguins.csv')
 summary(penguins)
 
 penguins_missing <- delete_MCAR(penguins,0.5,"flipper_length_mm")
+penguins_missing_all <- delete_MCAR(penguins,0.5,c("flipper_length_mm","bill_length_mm","bill_depth_mm","body_mass_g","species","sex"))
+tryCatch(
+  {
+    penguins_imputed_missing <- impute_lm(penguins_missing_all, flipper_length_mm ~ species + bill_length_mm + bill_depth_mm)
+    
+    penguins_imputed_missing[!complete.cases(penguins_imputed_missing), ]
+    
+    regg = lm(body_mass_g~ flipper_length_mm, data = penguins_missing_all,na.action = na.fail())
+    print(paste(summary(regg)$adj.r.squared, "is the R-Squared for the linear model where imputation of missing values was performed with another linear model before modeling."))
+  },
+  error = function(e) 
+  {
+    print("This caused an error!") # or whatever error handling code you want
+  }
+)
+
 
 ggplot(penguins, aes(body_mass_g, flipper_length_mm)) + 
   geom_count() + 
